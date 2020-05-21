ac215d42ad75e8116abee3a4df25fceb34a0c943
organometallica <marshall.r.brennan@gmail.com>
2020-05-21 08:56:42 -0500

    Updates

---
+++ precache-manifest.09b7abf988f388384e58505afa22b937.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600b205b9c636f6d2fb3b613400fde21",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "006082df8f56fe13b4d5",
+    "url": "/jammming/static/css/main.1eed67cf.chunk.css"
+  },
+  {
+    "revision": "8b4fbf57bd0d30cccbfd",
+    "url": "/jammming/static/js/2.7c66ebdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammming/static/js/2.7c66ebdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006082df8f56fe13b4d5",
+    "url": "/jammming/static/js/main.7d414765.chunk.js"
+  },
+  {
+    "revision": "881f10213c941e645fd7",
+    "url": "/jammming/static/js/runtime-main.34685740.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
